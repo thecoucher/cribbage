bcd8225a0e46a40aafc1e1aac1b505a590a220a6
couchp <thecoucher@gmail.com>
2020-01-27 22:33:36 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cribbage1/precache-manifest.c4979042b9ae09800528d5ee9ae5d43d.js"
+  "/cribbage/precache-manifest.42f2d1a9767fe8ea26593d390f4f76e5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cribbage1/index.html", {
+workbox.routing.registerNavigationRoute("/cribbage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
